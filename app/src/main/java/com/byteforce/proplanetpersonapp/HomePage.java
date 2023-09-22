67237d3607c078c2e9b3f18395516eb9082dafae
Govind Chaudhary <gs9012406068@gmail.com>
2023-09-23 00:37:33 +0530

    bottom navigation bar added

--- app/src/main/java/com/byteforce/proplanetpersonapp/HomePage.java
+++ app/src/main/java/com/byteforce/proplanetpersonapp/HomePage.java
@@ -1,8 +1,10 @@
 package com.byteforce.proplanetpersonapp;
 
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set the initial selected item
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+
+        // Set up a listener to handle item selection
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+
+                        // Replace code or fragment transaction
+                        return true;
+                    case R.id.my_plan:
+
+
+                        return true;
+                    case R.id.navigation_dashboard:
+
+
+                        return true;
+                    case R.id.navigation_our_misson:
+
+
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/byteforce/proplanetpersonapp/MainActivity.java
+++ app/src/main/java/com/byteforce/proplanetpersonapp/MainActivity.java
@@ -1,135 +1,58 @@
 package com.byteforce.proplanetpersonapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Context;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.FirebaseDatabase;
-
-
-import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
-    Button googleAuth;
-    FirebaseAuth auth;
-    FirebaseDatabase database;
-    GoogleSignInClient mGoogleSignInClient;
-    int RC_SIGN_IN = 20 ;
+    private static final String PREF_NAME = "MyAppPrefs";
+    private static final String PREF_FIRST_TIME = "isFirstTime";
+    private Button log_in;
+    private Button btnGoogleAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseApp.initializeApp(this);
-        googleAuth = findViewById(R.id.btnGoogleAuth);
+        log_in = findViewById(R.id.log_in);
+        btnGoogleAuth = findViewById(R.id.btnGoogleAuth);
 
-        auth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance();
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail().build();
-
-        mGoogleSignInClient= GoogleSignIn.getClient(this,gso);
-
-        googleAuth.setOnClickListener(new View.OnClickListener() {
+        btnGoogleAuth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                googleSignIn();
+                Toast.makeText(MainActivity.this, "This Option Is Under Testing/Production", Toast.LENGTH_SHORT).show();
             }
         });
 
-        if (auth.getCurrentUser() !=null){
-            Intent intent = new Intent(MainActivity.this ,HomePage.class);
-            startActivity(intent);
-            finish();
+            // Check if the app is opened for the first time
+            SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+            boolean isFirstTime = sharedPreferences.getBoolean(PREF_FIRST_TIME, true);
 
-        }
-
-
-    }
-
-    private void googleSignIn() {
-
-        Intent intent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(intent,RC_SIGN_IN);
-
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode==RC_SIGN_IN){
-
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firbaseAuth(account.getIdToken());
-
+            if (isFirstTime) {
+                sharedPreferences.edit().putBoolean(PREF_FIRST_TIME, false).apply();
+            } else {
+                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                startActivity(intent);
+                finish(); // I am Closing the MainActivity so the user can't navigate back to it
             }
-            catch (Exception e){
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }
 
-
+            log_in.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Thanks For Being A Pro Planet Person ", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, HomePage.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
 
     }
-
-    private void firbaseAuth(String idToken) {
-
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken ,null);
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if (task.isSuccessful()){
-                            FirebaseUser user = auth.getCurrentUser();
-
-                            HashMap<String,Object> map = new HashMap<>();
-                            assert user != null;
-                            map.put("id",user.getUid());
-                            map.put("name",user.getDisplayName());
-                            map.put("profile",user.getPhotoUrl().toString());
-
-                            database.getReference().child("users").child(user.getUid()).setValue(map);
-
-                            Intent intent = new Intent(MainActivity.this ,HomePage.class);
-                            startActivity(intent);
-
-                        }
-                        else {
-                            Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-
-
 }
